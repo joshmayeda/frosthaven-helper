0a088078c7fe547bc2a46ddbb16ef9b235137cef
mayeda <joshmayeda@gmail.com>
2023-09-27 23:50:22 -0700

    homepage temp button

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import LootDeckSelect from './lootdeck/LootDeckSelectModal'
+import React from 'react';
+import { Button } from '@mui/material';
 
 function HomePage() {
 
   return (
     <div className="overflow-y-auto overflow-x-hidden">
-      <main className="flex flex-col min-h-screen justify-between divide-y-1">
-        <LootDeckSelect />
+      <main className="flex flex-col min-h-screen justify-center items-center divide-y-1">
+        <Button variant="contained" className="bg-green-600 font-bold">
+          <a href="/lootdeck">Loot Deck Creator</a>
+        </Button>
       </main>
     </div>
   )
