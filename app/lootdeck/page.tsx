90dc6a8b9f97c4fcefb8f74c058698cfbc565223
mayeda <joshmayeda@gmail.com>
2023-09-29 12:32:45 -0700

    scroll fix test 1

--- app/lootdeck/page.tsx
+++ app/lootdeck/page.tsx
@@ -273,9 +273,9 @@ const LootDeck: React.FC = () => {
 
   return (
     <div className="overflow-y-hidden overflow-x-hidden">
-      <main className="flex min-h-screen divide-y-1 items-center justify-center overflow-hidden">
+      <main className="flex min-h-screen divide-y-1 items-center justify-center xs:overflow-hidden">
         <div className="flex flex-col xl:flex-row h-11/12 w-11/12 justify-center xl:gap-32">
-          <div className="relative self-center outline outline-white w-2/3 h-90 rotate-90 xs:h-72 xs:w-1/2 md:self-center md:h-128 md:w-5/12 xl:w-391 xl:h-600 xl:rotate-0 rounded-xl text-white text-center">
+          <div className="absolute xxs:top-12 xs:top-32 sm:relative sm:top-0 self-center outline outline-white w-2/3 h-90 rotate-90 xxs:h-72 xxs:w-1/2 md:self-center md:h-128 md:w-5/12 xl:w-391 xl:h-600 xl:rotate-0 rounded-xl text-white text-center">
             {lootDeck.length > 0 ? (
               <Button disableRipple onClick={drawLootCard} className="w-full h-full p-0">
                 <Image
@@ -320,7 +320,7 @@ const LootDeck: React.FC = () => {
               setScenarioCoins={setScenarioCoins}
               createLootDeck={createLootDeck}
             />
-          <div className="relative self-center outline outline-white w-2/3 h-90 rotate-90 xs:h-72 xs:w-1/2 md:self-center md:h-128 md:w-5/12 xl:w-391 xl:h-600 xl:rotate-0 rounded-xl text-white text-center">
+          <div className="absolute xxs:bottom-24 xs:bottom-32 sm:relative sm:bottom-0 self-center outline outline-white w-2/3 h-90 rotate-90 xxs:h-72 xxs:w-1/2 md:self-center md:h-128 md:w-5/12 xl:w-391 xl:h-600 xl:rotate-0 rounded-xl text-white text-center">
             {discardPile.length > 0 ? (
               <Image
                 src={discardPile[discardPile.length - 1].src}
