c3c8e68e0387ff8aa723ea137b3756cbab03aeab
mayeda <joshmayeda@gmail.com>
2023-09-28 21:24:30 -0700

    testing zoom

--- app/lootdeck/layout.tsx
+++ app/lootdeck/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=0.9" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
